75cd972cadd295b49fba58ff871793bba521c2ca
an nguyen <hello.an.ng@gmail.com>
2017-01-19 11:02:10 -0800

    binds enter keypress to add button

--- public/main.js
+++ public/main.js
@@ -8,29 +8,44 @@ $input.focus();
 var $ul = $('.todos');
 var $btn = $('.add');
 var $input = $('input');
+var $span = $('span');
 
-// APPEND TO-DOS
-// on button click
-$btn.click(function(evt) {
+// binds keypress to add button
+$(document).bind('keypress', function(evt) {
+  if (evt.which === 13) {
+    $btn.trigger('keypress');
+    addToDo();
+  };
+});
+
+// ADD TO-DOS
+function addToDo(evt) {
   // store input value into todo variable
   var todo = $input.val();
   // if input value is NOT empty
   if (todo !== "") {
     // post request to /posts
-    $.post('/posts', { todo: todo}, function(res){
+    $.post('/posts', { todo: todo }, function(res) {
       // console.log(evt);
       // console.log(evt.target);
       // console.log(todo);
       $input.focus();
       getPosts();
-    })
-  }
-})
+    });
+  };
+};
 
+// UPDATE TO-DOS
+$span.keypress(function(evt) {
+  if (evt.which === 13) {
+    alert('enter key pressed!');
+  };
+});
+
+// AJAX GET request to /posts and responds w/ data
 function getPosts() {
-  console.log('getPosts is running');
   $.get('/posts', function(res){
-    console.log("res=", res);
+    // console.log("res=", res);
     renderList(res);
   });
 };
@@ -38,7 +53,7 @@ getPosts();
 
 function renderList(list) {
   var htmlTemp = $('#todo-template').html();
-  console.log(htmlTemp);
+  // console.log(htmlTemp);
   var template = Handlebars.compile(htmlTemp); // returns a function
   var html = template(list);
   // console.log(html);
